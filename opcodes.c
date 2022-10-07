4e1426a531f7d4fdd34561c0c95f4dd2b0040f46
sumutoni <steumutoni@gmail.com>
2022-10-07 00:39:17 -0700

    removed newline

--- opcodes.c
+++ opcodes.c
@@ -90,7 +90,7 @@ void swap(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack) || (!(*stack)->next && !(*stack)->prev))
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n",
-				line_numbe);
+				line_number);
 		exit(EXIT_FAILURE);
 	}
 	cur = *stack;
